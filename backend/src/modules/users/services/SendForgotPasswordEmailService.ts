51bdbb7595d81e4a8bdb37a531960bb48ebc5380
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-12-18 00:50:46 -0300

    start to create send forgot email service

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+
+// import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute(): Promise<void> {}
+}
+
+export default SendForgotPasswordEmailService;
