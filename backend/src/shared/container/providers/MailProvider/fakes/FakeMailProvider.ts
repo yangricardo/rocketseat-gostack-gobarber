438a9e3ea6710653fa2bcb8dbb866914dda5ca85
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-12-18 01:03:35 -0300

    create FakeMailProvider

---
+++ backend/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -0,0 +1,14 @@
+import IMailProvider from '../implementations/IMailProvider';
+
+interface IMessage {
+  to: string;
+  body: string;
+}
+
+export default class FakeMailProvider implements IMailProvider {
+  private messages: IMessage[] = [];
+
+  async sendMail(to: string, body: string): Promise<void> {
+    this.messages.push({ to, body });
+  }
+}
